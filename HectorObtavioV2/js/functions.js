1e4a34ef8e9970f150cc4dcf2582dd8770fcc43b
HectorObtavio <Hectorobtavio@gmail.com>
2017-11-28 15:43:13 -0800

    Add files via upload

---
+++ HectorObtavioV2/js/functions.js
@@ -0,0 +1,12 @@
+$(window).scroll(function(){
+  console.log('hi')
+
+
+    var header = $('.navtop');
+
+$('.navtop').each(function('.navtop'){
+  setTimeout(function(){
+    $('.navtop').eq('navtop').addClass('');
+  }, 200 * i);
+
+})
